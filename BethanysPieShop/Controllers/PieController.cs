15a33715e05b216b77ebc8439ef9a919241616a2
Ian <ianmaciel76@gmail.com>
2021-04-13 08:59:53 -0300

    Adicionando atividade do modulo 6

--- BethanysPieShop/Controllers/PieController.cs
+++ BethanysPieShop/Controllers/PieController.cs
@@ -33,5 +33,13 @@ namespace BethanysPieShop.Controllers
             piesListViewModel.CurrentCategory = "Cheese cakes";
             return View(piesListViewModel);
         }
+
+        public IActionResult Details(int id)
+        {
+            Pie pie = _pieRepository.GetPieById(id);
+            if (pie == null)
+                return NotFound();
+            return View(pie);
+        }
     }
 }
