c2b9e4aa389617844c6f62ee79460ffef54fb0be
Ian <ianmaciel76@gmail.com>
2021-04-16 09:58:40 -0300

    Adicionando atividade do modulo 8

---
+++ BethanysPieShop/Models/PieGiftOrder.cs
@@ -0,0 +1,10 @@
+﻿namespace BethanysPieShop.Models
+{
+    public class PieGiftOrder
+    {
+        public int PieGiftOrderId { get; set; }
+        public Pie Pie { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+    }
+}
